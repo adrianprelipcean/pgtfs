375a4439ed0230207e214681bd8266b419a8c852
Adrian <adrianprelipceanc@gmail.com>
2024-04-19 11:10:11 +0200

    test: trips where the sequence is broken

--- sql/pgtfs_test.sql
+++ sql/pgtfs_test.sql
@@ -24,6 +24,26 @@ select trip_id, stop_id, arrival_time::double precision, departure_time::double 
 $$
 );
 
+-- simple connectivity test where sequence has a gap (3 is missing)
+select
+    stop_id,
+    stop_sequence,
+    arrival_time,
+    trip_id
+from pgtfs_csa(
+    '1', '4', 1712707200.0,
+    $$
+select trip_id, stop_id, arrival_time::double precision, departure_time::double precision, stop_sequence::int from (
+    values
+     ('1753','1',NULL,1712757660,1),
+     ('1753','2',1712759880,1712759940,2),
+     ('1753','3',1712762040,1712762100,4),
+     ('1753','4',1712762100,NULL,5)
+    )
+    as data (trip_id, stop_id, arrival_time, departure_time, stop_sequence);
+$$
+);
+
 -- connected and route on single trip
 select
     stop_id,
